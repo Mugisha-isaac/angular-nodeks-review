845e21d5703c0c28f1c208c91621d566c7cea31c
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-09-17 16:34:10 +0200

    [controllers/controller.js] defining all  required functions

---
+++ backend/controllers/controller.js
@@ -0,0 +1,45 @@
+const db = require("../model");
+const Tutorial = db.tutorials;
+const Op = db.Sequelize.Op;
+
+// creating and saving new tutorial
+
+exports.create = (req,res)=>{
+
+}
+
+// retrieving all tutorials from database
+
+exports.findAll = (req,res)=>{
+
+}
+
+// finding single tutorial with id
+
+exports.findOne = (req,res)=>{
+
+}
+
+// updating single tutorial with id as request
+
+exports.update = (req,res)=>{
+
+}
+
+// deleting a tutorial with id as a request
+
+exports.delete = (req,res)=>{
+
+}
+
+// deleting all tutorials from database
+
+exports.deleteAll = (req,res)=>{
+
+}
+
+// finding all published tutorials
+
+exports.findAllPublished = (req,res)=>{
+    
+}
