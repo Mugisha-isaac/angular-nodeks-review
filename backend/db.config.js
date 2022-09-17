d380faaad855c54c7bb8eeb87f252aa91c81e849
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-09-17 15:59:23 +0200

    [feat: db.config.js]

---
+++ backend/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST:'localhost',
+    USER:'isaac',
+    PASSWORD:'Mugisha12!',
+    DB:'angular',
+    dialect:'mysql',
+    pool:{
+        max:5,
+        min:0,
+        acquire:3000,
+        idle:1000
+    }
+}
