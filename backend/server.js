e6d2042f7afc625bcc496e22734b35003cea3885
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-09-17 15:47:32 +0200

    [feat: server.js] setting up express backend application

--- backend/server.js
+++ backend/server.js
@@ -4,5 +4,26 @@ const cors = require('cors');
 const app = express();
 
 const corsOptions = {
-    
+    origin:'http://locahost/8081'
 }
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+// initial route
+
+app.get("/",(req,res)=>{
+    res.json({message:"Welcome to angular+nodejs review"})
+});
+
+
+// setting and listening to the port
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`);
+})
